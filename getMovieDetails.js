df955f46735ce737a451e909bde48b25b8dc5561
raecello <37798815+raecello@users.noreply.github.com>
2018-09-27 19:37:42 +1000

    Update getMovieDetails.js

--- getMovieDetails.js
+++ getMovieDetails.js
@@ -1,3 +1,17 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const API_KEY = require('./apiKey');
+
+const server = express();
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.use(bodyParser.json());
+
 server.post('/get-movie-details', (req, res) => {
 
     const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
@@ -25,4 +39,8 @@ server.post('/get-movie-details', (req, res) => {
             source: 'get-movie-details'
         });
     });
+});
+
+server.listen((process.env.PORT || 8000), () => {
+    console.log("Server is up and running...");
 });
