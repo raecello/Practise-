aad494030096838ed81c3c76fff747ae09a80054
raecello <37798815+raecello@users.noreply.github.com>
2018-09-27 23:27:36 +1000

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.use(bodyParser.json());
 
 server.post('/get-movie-details', (req, res) => {
 
-    const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'Gladiator';
+    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'Gladiator';
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=d5dbbae7`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
